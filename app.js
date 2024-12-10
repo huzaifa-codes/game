88fbcdf026daf1beb65b0fd86cb2c66c398882af
huzaifa-codes <huzaifabusiness60@gmail.com>
2024-12-11 04:04:13 +0500

    game

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let main = document.querySelector("#me")
 let you = document.querySelector("#you")
+let div = document.querySelector(".con")
 let sum = ["rock" , "papper" , "Scissors"]
 
 let com = 0
@@ -8,7 +9,10 @@ function gamer(han){
     let computer = sum[Math.floor(Math.random() * sum.length)]
 
     if(han === computer){
+        div.innerHTML = ""
         console.log("ye drwo ho gaya hai");
+        div.innerHTML = `<h1> It Was Dow</h1>`
+
         
     }
     else if( han === "rock" && computer === "Scissors" ||  han === "papper" && computer === "rock" || han === "Scissors" && computer === "papper"){
@@ -16,11 +20,13 @@ function gamer(han){
 
         console.log("you win");
         console.log(me);
+        div.innerHTML = `<h1>You Win</h1>`
         
         
     }
     else{
         console.log("you lose");
+         div.innerHTML = `<h1>You Loss</h1>`
         
         you.innerHTML = com++
         console.log(com);
